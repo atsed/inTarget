a686c2390cfd59b2853498e453757048ca187bbd
atsed <theatsed@gmail.com>
2021-06-15 20:07:46 +0300

    edit: small changes

---
+++ inTargetTests/inTargetTests.swift
@@ -0,0 +1,32 @@
+//
+//  inTargetTests.swift
+//  inTargetTests
+//
+//  Created by Desta on 15.06.2021.
+//
+
+import XCTest
+
+class inTargetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
