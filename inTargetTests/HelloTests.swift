7f6c59321340f342b64f8ec08b06074aa9690904
atsed <theatsed@gmail.com>
2021-06-15 23:02:38 +0300

    edit: small changes

---
+++ inTargetTests/HelloTests.swift
@@ -0,0 +1,16 @@
+//
+//  HelloTests.swift
+//  inTargetTests
+//
+//  Created by Desta on 15.06.2021.
+//
+
+import XCTest
+
+class HelloTests: XCTestCase {
+
+    func testHello() {
+        return
+    }
+
+}
